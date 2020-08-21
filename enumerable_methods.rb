434f78e31ee82034884157aa12a7fa75054d6200
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-08-21 18:45:19 -0400

    Add my_any? & my_none? methods

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,7 +1,7 @@
 # rubocop:disable Style/CaseEquality
 array = [1,2,3,4,5,6,7,8,9]
 hash = {one: 1, two: 2, three: 3}
-other_array = ["Testing", "how", "this", "array", "thing", "works"]
+other_array = ["Testing", "how", "this", "array", "thing", "works", "Happiness"]
 other_hash = {desperation: 1, testing: 2, achieving: 3, figuring_out: 4}
 
 module Enumerable
@@ -40,7 +40,7 @@ module Enumerable
     end
   end #Second Method ends here
 
-  def my_select # Third method
+  def my_select #Third method
     unless block_given?
       return puts "Enumerator #{self}:my_select"
     else
@@ -55,9 +55,9 @@ module Enumerable
         new_arr
       else 
         new_hash = {}
-        for i in 0...(self.length)
         key = self.keys
         value = self.values
+        for i in 0...(self.length)
           if yield(key[i], value[i])
             new_hash.store(key[i], value[i])
           end
@@ -65,20 +65,77 @@ module Enumerable
         new_hash
       end
     end
-  end # Third method ends here
+  end #Third method ends here
 
-  def my_all #Fourth method
-    unless block_given?
+  def my_all? #Fourth method
+    if !block_given?
       return true
-    else
+    elsif self.is_a? Array
       for i in 0...(self.length)
         unless yield(self[i])
           return false
         end
       end
       return true
+    else 
+      key = self.keys
+      value = self.values
+      for i in 0...(self.length)
+        unless yield(key[i], value[i])
+          return false
+        end
+      end
+      return true
     end
-  end # Fourth method ends here
-end
+  end #Fourth method ends here
 
-puts array.my_all {|i| i > 5}
+  def my_any? #Fifth method
+    if !block_given?
+      return true
+    elsif self.is_a? Array
+      for i in 0...(self.length)
+        if yield(self[i])
+          return true
+        end
+      end
+      return false
+    else 
+      key = self.keys
+      value = self.values
+      for i in 0...(self.length)
+        if yield(key[i], value[i])
+          return true
+        end
+      end
+      return false
+    end
+  end #Fifth method ends here
+
+  def my_none? #Sixth method
+    if !block_given? 
+      return false
+    elsif self.is_a? Array
+      for i in 0...(self.length)
+        if yield(self[i])
+          return false
+        end
+      end
+      return true
+    else 
+      key = self.keys
+      value = self.values
+      for i in 0...(self.length)
+        if yield(key[i], value[i])
+          return false
+        end
+      end
+      return true
+    end
+  end #Sixth method ends here
+
+  def my_count #Seventh method  
+    if !block_given?
+      return self.length 
+    end
+  end #Seventh method ends here
+end
