02d2af984d6319139bc0b4a5f04b473c1a3d7c8c
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-08-26 13:50:05 -0400

    Fix remainig methods

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength,Metrics/MethodLength
+# rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength,
 
 module Enumerable
   # 1st method
@@ -15,7 +15,7 @@ module Enumerable
           yield(hash[i])
         end
       end
-      return self
+      self
     else
       puts "Enumerator #{self}:my_each"
     end
@@ -36,7 +36,7 @@ module Enumerable
           yield(hash[j], j)
         end
       end
-      return self
+      self
     else
       puts "Enumerator #{self}:my_each_with_index"
     end
@@ -69,7 +69,7 @@ module Enumerable
 
   # 4th method
   def my_all?(arg = nil)
-    return true if self.length == 0
+    return true if empty?
 
     if arg.nil? and !block_given?
       (0...(length)).each do |i|
@@ -77,61 +77,80 @@ module Enumerable
       end
     elsif arg.is_a? Class
       (0...(length)).each do |i|
-        return false if !self[i].is_a? arg 
+        return false unless self[i].is_a? arg
       end
     elsif arg.is_a? Regexp
       (0...(length)).each do |i|
-        return false if !self[i].match(arg)
+        return false unless self[i].match(arg)
       end
     elsif !arg.is_a? Regexp and !arg.is_a? Class and !block_given?
       (0...(length)).each do |i|
         return false if self[i] != arg
       end
-    else 
+    else
       (0...(length)).each do |i|
-        puts "it entered here"
-        return false if !yield(self[i])
+        puts 'it entered here'
+        return false unless yield(self[i])
       end
     end
     true
   end
 
   # 5th method
-  def my_any?
-    if !block_given?
-      true
-    elsif is_a? Array
+
+  def my_any?(arg = nil)
+    return false if empty?
+
+    if arg.nil? and !block_given?
+      (0...(length)).each do |i|
+        return true if self[i] == true
+      end
+    elsif arg.is_a? Class
+      (0...(length)).each do |i|
+        return true if self[i].is_a? arg
+      end
+    elsif arg.is_a? Regexp
+      (0...(length)).each do |i|
+        return true if self[i].match(arg)
+      end
+    elsif !arg.is_a? Regexp and !arg.is_a? Class and !block_given?
+      (0...(length)).each do |i|
+        return true if self[i] == arg
+      end
+    else
       (0...(length)).each do |i|
         return true if yield(self[i])
       end
-      false
-    else
-      key = keys
-      value = values
-      (0...(length)).each do |i|
-        return true if yield(key[i], value[i])
-      end
-      false
     end
+    false
   end
 
   # 6th method
-  def my_none?
-    if !block_given?
-      false
-    elsif is_a? Array
+  def my_none?(arg = nil)
+    return true if empty?
+
+    if arg.nil? and !block_given?
+      (0...(length)).each do |i|
+        return false if self[i] == true
+      end
+    elsif arg.is_a? Class
+      (0...(length)).each do |i|
+        return false if self[i].is_a? arg
+      end
+    elsif arg.is_a? Regexp
+      (0...(length)).each do |i|
+        return false if self[i].match(arg)
+      end
+    elsif !arg.is_a? Regexp and !arg.is_a? Class and !block_given?
+      (0...(length)).each do |i|
+        return false unless self[i] != arg
+      end
+    else
       (0...(length)).each do |i|
         return false if yield(self[i])
       end
-      true
-    else
-      key = keys
-      value = values
-      (0...(length)).each do |i|
-        return false if yield(key[i], value[i])
-      end
-      true
     end
+    true
   end
 
   # 7th method
@@ -185,36 +204,52 @@ module Enumerable
 
   # 9th method
   def my_inject(arg = nil, num = nil)
-
     to_array = Array(self)
-    to_array.unshift(arg) unless arg.nil?
+    to_array.unshift(arg) unless arg.nil? or arg.is_a? Symbol
     injection = to_array[0]
+
     if num.nil? and block_given?
       (1...to_array.length).each do |i|
         injection = yield(injection, to_array[i])
       end
-    elsif num.is_a? Symbol or !block_given?
-      arg = arg.to_sym
-      puts "arg #{arg} #{arg.class}"
+    elsif num.is_a? Symbol and !block_given?
       case num
       when :+
         (1...to_array.length).each do |i|
-          puts injection += to_array[i]
+          injection += to_array[i]
         end
       when :-
         (1...to_array.length).each do |i|
-          puts injection -= to_array[i]
+          injection -= to_array[i]
         end
       when :*
         (1...to_array.length).each do |i|
-          puts "injection #{injection}"
-          puts injection *= to_array[i]
+          injection *= to_array[i]
         end
       when :/
         (1...to_array.length).each do |i|
-          puts injection /= to_array[i]
+          injection /= to_array[i]
         end
-      end 
+      end
+    elsif arg.is_a? Symbol and !block_given?
+      case arg
+      when :+
+        (1...to_array.length).each do |i|
+          injection += to_array[i]
+        end
+      when :-
+        (1...to_array.length).each do |i|
+          injection -= to_array[i]
+        end
+      when :*
+        (1...to_array.length).each do |i|
+          injection *= to_array[i]
+        end
+      when :/
+        (1...to_array.length).each do |i|
+          injection /= to_array[i]
+        end
+      end
     else
       injection = num
       (0...to_array.length).each do |i|
@@ -225,9 +260,6 @@ module Enumerable
   end
 
   # 10th method
-  def multiply_els
-    my_inject { |i, j| i * j }
-  end
 
   # This code was left as a comment on purpose
   # so you can see the progression of the my_map method
@@ -286,4 +318,8 @@ module Enumerable
   end
 end
 
-# rubocop:enable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength,Metrics/MethodLength
+def multiply_els(arr)
+  arr.my_inject(:*)
+end
+
+# rubocop:enable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity,Metrics/ModuleLength
